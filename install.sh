2215a36271b55675da9c5944399d365264240ab8
Alex Rea <git@area.io>
2015-03-05 22:37:26 +0000

    Account for spaces in path in install script

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-cp $DIR/com.area.gamecubeAdapter.plist /Library/LaunchAgents/
+cp "$DIR/com.area.gamecubeAdapter.plist" /Library/LaunchAgents/
 chown root /Library/LaunchAgents/com.area.gamecubeAdapter.plist
 chgrp wheel /Library/LaunchAgents/com.area.gamecubeAdapter.plist
 
-cp -r $DIR/wiiu-gc-adapter /Applications/
+cp -r "$DIR/wiiu-gc-adapter" /Applications/
 chown -R root:wheel /Applications/wiiu-gc-adapter/Frameworks/WirtualJoy.framework
 
 cp -r "$DIR/WiiuGCCAdapter.kext" /Library/Extensions/
